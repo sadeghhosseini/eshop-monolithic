b33a4d1557a299d1e5743a00f1743f808a9e7fc5
Sadegh Hosseini <s.hosseini@develop.local>
2021-12-10 21:14:25 +0330

    seeds categories and products tables in DatabaseSeeder

--- eshop/database/seeders/DatabaseSeeder.php
+++ eshop/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,114 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call([
+        /* $this->call([
             UserSeeder::class,
             ProductSeeder::class,
-        ]);
+        ]); */
+
+        $categoriesTable = 'categories';
+        $productsTable = 'products';
+        $data = [
+            $categoriesTable => CategoryTable::populate(),
+            $productsTable => ProductTable::populate(),
+        ];
+
+        foreach($data as $table => $records) {
+            foreach($records as $record) {
+                DB::table($table)->insert((array)$record);
+            }
+        }
     }
 }
+
+
+class CategoryTable {
+    function __construct($id, $title, $description, $parent_id = null) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->parent_id = $parent_id;
+    }
+
+    static function populate() {
+        return [
+            new CategoryTable(
+                1, 
+                'لوازم الکتریکی',
+                'انواع لوازم الکتریکی با بهترین کیفیت',
+                ),
+            new CategoryTable(
+                2, 
+                'مواد غذایی',
+                'انواع مواد غذایی تازه و با کیفیت',
+            ),
+            new CategoryTable(
+                3,
+                'لپ‌تاپ',
+                'انواع لپتاپ‌های گیمینگ و اداری و خانگی', 
+                1,
+            ),
+            new CategoryTable(
+                4, 
+                'لپ‌تاپ گیمینگ', 
+                'بهترین لپتاپ‌های گیمینگ',
+                3
+            ),
+            new CategoryTable(
+                5, 
+                'لپ‌تاپ اداری', 
+                'بهترین لپتاپ‌های اداری',
+                3,
+            ),
+        ];
+    }
+}
+
+class ProductTable {
+    function __construct($id, $title, $description, $quantity, $price, $category_id) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->quantity = $quantity;
+        $this->price = $price;
+        $this->category_id = $category_id;
+    }
+
+    static function populate() {
+        return [
+            new ProductTable(
+                1,
+                'لپتاپ ایسوس',
+                'لپتاپ های مقاوم و معتبر',
+                4,
+                32000000,
+                3
+            ),
+            new ProductTable(
+                2,
+                'لپتاپ سونی vio',
+                'لپتاپ‌های مقاوم از شرکت سونی',
+                15,
+                50000000,
+                5
+            ),
+            new ProductTable(
+                3,
+                'لپتا legion 5 lenovo',
+                'لپتاپ گیمینگ از شرکت لنوو',
+                10,
+                65000000,
+                4,
+            ),
+            new ProductTable(
+                4,
+                'لامپ هوشمند شیائومی',
+                'لامپ‌های هوشمند شیائومی با دوام بالا و دارای ریموت کنترل',
+                50,
+                100000,
+                1,
+            ),
+        ];
+    }
+}
+
