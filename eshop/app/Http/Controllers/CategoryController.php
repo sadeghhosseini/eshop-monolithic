2609e5786ee0791a649b6a9546b3e90fd7e95155
Sadegh Hosseini <s.hosseini@develop.local>
2021-12-16 15:59:06 +0330

    defines routes and corresponding controller actions for category entity

--- eshop/app/Http/Controllers/CategoryController.php
+++ eshop/app/Http/Controllers/CategoryController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+
+    public function create(CreateCategoryRequest $request)
+    {
+        $newCategory = Category::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+        ]);
+
+        return response()->json($newCategory);
+    }
+
+    public function getAll()
+    {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    public function get(Category $category)
+    {
+        return response()->json($category);
+    }
+
+    public function delete(Category $category)
+    {
+        $category->delete();
+        return response()->json([]);
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $category->title = $request->input('title', $category->title);
+        $category->description = $request->input('description', $category->description);
+        $category->save();
+        return response()->json($category);
+    }
 }
--- eshop/routes/api.php
+++ eshop/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/categories', [CategoryController::class, 'create']);
+Route::get('/categories', [CategoryController::class, 'getAll']);
+Route::get('/categories/{category}', [CategoryController::class, 'get']);
+Route::delete('/categories/{category}', [CategoryController::class, 'delete']);
+Route::patch('/categories/{category}', [CategoryController::class, 'update']);
