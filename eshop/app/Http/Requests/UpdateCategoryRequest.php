d4827d5ca2a9b48e1531a336bdf863f8768af75a
Sadegh Hosseini <s.hosseini@develop.local>
2021-12-25 15:13:27 +0330

    changes validation rule for title from nullable to required

--- eshop/app/Http/Requests/UpdateCategoryRequest.php
+++ eshop/app/Http/Requests/UpdateCategoryRequest.php
@@ -24,7 +24,7 @@ class UpdateCategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => ['nullable'],
+            'title' => ['required'],
             'description' => ['nullable'],
         ];
     }
