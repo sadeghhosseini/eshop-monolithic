de50b8434b8b8894008df6554db775d707ab74a5
Sadegh Hosseini <s.hosseini@develop.local>
2021-12-29 11:22:46 +0330

    refactors tests in CategoriesTest

--- eshop/tests/Feature/CategoriesTest.php
+++ eshop/tests/Feature/CategoriesTest.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\Category;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Model;
 
 use function Pest\Laravel\delete;
 use function Pest\Laravel\get;
@@ -14,107 +14,130 @@ use function Tests\helpers\endpoint;
 use function Tests\helpers\group;
 use function Tests\helpers\mit;
 
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\TestResponse;
+
 uses(RefreshDatabase::class);
 
+
+/* expect($response)->toEqualAll($categories, function ($responseItem, $expectedItem) {
+            expect($responseItem->title)->toEqual($expectedItem->title);
+            expect($responseItem->description)->toEqual($expectedItem->description);
+            expect($responseItem->id)->toEqual($expectedItem->id);
+        }); 
+*/
+expect()->extend('toEqualAll', function ($expectedItems, $expectationClosure) {
+    /** @var Model[] $expectedItems */
+    /** @var  TestResponse $response */
+    $response = $this->value;
+    $responseItems = json_decode($response->baseResponse->content());
+    foreach ($expectedItems as $expectedItem) {
+        $respItem = array_filter($responseItems, function ($value) use ($expectedItem) {
+            return $expectedItem->id == $value->id;
+        });
+        if (empty($respItem)) {
+            // expect($expectedItem->id)->toEqual(null);
+            throw new Exception(
+                "response item with id of {$expectedItem->id} does not exist"
+            );
+        }
+        $respItem = array_pop($respItem);
+        if ($expectationClosure) {
+            $expectationClosure($respItem, $expectedItem);
+        }
+    }
+
+    /* foreach($items as $item) {
+        echo $item->title;
+    } */
+});
+
 //Read
-group('GET', '/api/categories', function($verb, $url) {
-    
+group('GET', '/api/categories', function ($verb, $url) {
+
     mit('returns 200 if no categories exist', function () use ($url) {
         $response = get($url);
         $response->assertStatus(200);
     }, $verb, $url);
-    
+
     mit('returns empty array, if category table is empty', function ()  use ($url) {
         $response = get($url);
         $response->assertExactJson([]);
     }, $verb, $url);
-    
+
     mit('returns all the category records in db', function ()  use ($url) {
-        $result = createRecords(Category::class, [
-            [
-                'title' => 'Laptop',
-                'description' => 'Best laptops ever',
-            ],
-            [
-                'title' => 'PC',
-                'description' => 'Best pcs ever',
-            ],
-            [
-                'title' => 'Mobile Phones',
-                'description' => 'Best phones in the market',
-            ],
-        ]);
+        $count = 1;
+        $categories = Category::factory()
+            ->count($count)
+            ->create();
+
         $response = get($url);
+        $response->assertJsonCount($count);
+
+        $responseItemsAsArray = $response->json();
+        $expectedItemsAsArray = $categories->toArray();
+        expect($responseItemsAsArray)
+            ->toEqualCanonicalizing($expectedItemsAsArray);
         
-        foreach ($result->getItems() as $category) {
-            $response->assertJsonFragment($category);
-        }
-        $response->assertJsonCount(count($result->getItems()));
     }, $verb, $url);
-    
 });
 
-group('GET', '/api/categories/{id}', function($verb, $url) {
+group('GET', '/api/categories/{id}', function ($verb, $url) {
     // $genUrl = fn ($id) => "/api/categories/${id}";
-    mit('returns 404 if no category with the id of {id} exists', function () use ($url){
+    mit('returns 404 if no category with the id of {id} exists', function () use ($url) {
         $response = get(buildUrl($url, ['id' => 1]));
         expect($response->baseResponse->content())->toBeJson();
         $response->assertStatus(404);
     }, $verb, $url);
 
-
     mit('returns the category with id = {id}', function () use ($url) {
-        $result = createRecords(Category::class, [
-            [
-                'title' => 'title-1',
-                'description' => 'description-1',
-            ]
-        ]);
-
-        $createdCategoryModel = $result->lastModel;
-        $response = get(buildUrl($url, ['id' => $createdCategoryModel->id]));
+        $category = Category::factory()->create();
+        $response = get(buildUrl($url, ['id' => $category->id]));
         expect($response->baseResponse->content())->toBeJson();
         $response->assertOk();
-        $response->assertJsonFragment($result->lastItem);
+        
+        $expectedItemsAsArray = $category->toArray();
+        expect($response->json())->toEqualCanonicalizing($expectedItemsAsArray);
     }, $verb, $url);
 });
 
 //Create
-group('POST', '/api/categories', function($verb, $url) {
-    mit('creates a user', function () use ($url) {
-        $response = post($url, [
-            'title' => 'title-1',
-            'description' => 'description-1',
-        ]);
-        $newCategory = json_decode($response->baseResponse->content());
-        $category = Category::find($newCategory->id);
-        expect($category->id)->toEqual($newCategory->id);
-        expect($category->title)->toEqual($newCategory->title);
-        expect($category->description)->toEqual($newCategory->description);
+group('POST', '/api/categories', function ($verb, $url) {
+    mit('creates a category without parent', function () use ($url) {
+        $category = Category::factory()->make();
+        $response = post($url, $category->toArray());
+        $response->assertOk();
+        $expectedItemsAsArray = $category->makeHidden('parent_id')->toArray();
+        expect($response->json())
+            ->toMatchArray($expectedItemsAsArray);
+    }, $verb, $url);
+    
+    mit('creates a category with parent', function () use ($url) {
+        $parentCategory = Category::factory()->create();
+        $category = Category::factory(['parent_id' => $parentCategory->id])->make();
+        $response = post($url, $category->toArray());
+        $response->assertOk();
+        $expectedResponseItemAsArray = $response->json();
+        $expectedItemsAsArray = $category->toArray();
+        expect($expectedResponseItemAsArray)
+            ->toMatchArray($expectedItemsAsArray);
     }, $verb, $url);
 
     mit('returns 400 if input data is not valid', function () use ($url) {
-        $response = post($url, [
-            'title' => '',
-            'description' => '',
-        ]);
-
+        $category = Category::factory(['title' => ''])->make();
+        $response = post($url, $category->toArray());
         $response->assertStatus(400);
     }, $verb, $url);
 });
 
 //Update
-group('PATCH', '/api/categories/{id}', function($verb, $url) {
+group('PATCH', '/api/categories/{id}', function ($verb, $url) {
     mit('returns 400 if input is not valid', function () use ($url) {
-        $result = createRecords(Category::class, [
-            [
-                'title' => 'title-1',
-                'description' => 'description-1',
-            ]
-        ]);
+        $category = Category::factory()->create();
 
         $response = patch(
-            buildUrl($url, ['id' => $result->lastModel->id]),
+            buildUrl($url, ['id' => $category->id]),
             ['title' => '']
         );
         $response->assertStatus(400);
@@ -126,41 +149,28 @@ group('PATCH', '/api/categories/{id}', function($verb, $url) {
     }, $verb, $url);
 
     mit('returns updated category record', function () use ($url) {
-        $result = createRecords(Category::class, [
-            [
-                'title' => 'title-1',
-                'description' => 'description-1',
-            ]
-        ]);
+        $category = Category::factory()->create();
 
-        $response = patch(buildUrl($url, ['id' => $result->lastModel->id]), ['title' => 'updated-title-1']);
+        $newTitle = 'updated-title';
+        $response = patch(buildUrl($url, ['id' => $category->id]), ['title' => $newTitle]);
+        $category->title = $newTitle;
         $response->assertOk();
-        $response->assertJsonFragment(
-            array_merge(
-                $result->lastModel->attributesToArray(),
-                ['title' => 'updated-title-1']
-            )
-        );
+        expect($response->json())
+            ->toMatchArray($category->toArray());
     }, $verb, $url);
 });
 
 //Delete
-group('DELETE', '/api/categories/{id}', function($verb, $url) {
+group('DELETE', '/api/categories/{id}', function ($verb, $url) {
     mit('delete - returns 404 if no category with id={id} exists', function () use ($url) {
         $response = delete(buildUrl($url, ['id' => 300]));
         $response->assertStatus(404);
     }, $verb, $url);
 
     mit('delete - returns 200 if category is deleted successfully', function () use ($url) {
-        $result = createRecords(Category::class, [
-            [
-                'title' => 'title-1',
-                'description' => 'description-1',
-            ]
-        ]);
-        $savedCategory = $result->lastModel;
-        $response = delete(buildUrl($url, ['id' => $savedCategory->id]));
+        $category = Category::factory()->create();
+        $response = delete(buildUrl($url, ['id' => $category->id]));
+        expect(Category::find($category->id))->toBeNull();
         $response->assertOk();
     }, $verb, $url);
 });
-
