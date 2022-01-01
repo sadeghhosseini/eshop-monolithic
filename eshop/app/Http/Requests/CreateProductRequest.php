a7f8ffc2304ab36d9a8816c09f5b6de032f57da5
Sadegh Hosseini <s.hosseini@develop.local>
2022-01-01 16:25:48 +0330

    creates a FormRequest class for product-creation endpoint and adds validation rules to it for request validation

---
+++ eshop/app/Http/Requests/CreateProductRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Category;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\Property;
+use App\Rules\ForeignKeyExists;
+use App\Rules\MinWord;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'min:3'],
+            'description' => ['nullable', new MinWord(3)],
+            'quantity' => ['required', 'gte:0'],
+            'price' => ['required'],
+            'category_id' => ['required', new ForeignKeyExists(Category::class)],
+            'new_images' => ['nullable'],
+            'new_images.*' => ['file'],
+            'image_ids' => ['nullable', new ForeignKeyExists(Image::class)],
+            'property_ids' => ['nullable', new ForeignKeyExists(Property::class)],
+            'new_properties' => ['nullable', 'array', 'min:1'],
+            'new_properties.*' => [
+                'string',
+                'min:2',
+                function ($attribute, $value, $fail) {
+                    $this->category_id;
+                    if (
+                        Property::where('category_id', $this->category_id)
+                        ->where('title', $value)
+                        ->exists()
+                    ) {
+                        $fail("Property with id of ${value} already exists for category with id of $this->category_id");
+                    }
+                }
+            ]
+        ];
+    }
+}
