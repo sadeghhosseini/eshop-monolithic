15fd4d95e19046943a6edde3417b9d46a87671d4
Sadegh Hosseini <s.hosseini@develop.local>
2022-01-04 12:45:26 +0330

    completes the implementation of update action

--- eshop/app/Http/Controllers/ProductController.php
+++ eshop/app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 use App\Models\Image;
 use App\Models\Product;
 use App\Models\Property;
@@ -32,11 +33,11 @@ class ProductController extends Controller
         $product->category_id = $request->category_id;
         $product->save();
 
-        $paths = [];
         if ($request->has('image_ids')) {
             $product->images()->attach($request->image_ids);
         }
         if ($request->has('new_images')) {
+            $paths = [];
             collect($request->new_images)->each(function ($image) use (&$paths) {
                 $paths[] = Storage::putFile('images', $image);
             });
@@ -50,7 +51,6 @@ class ProductController extends Controller
             $product->properties()->attach($request->property_ids);
         }
         if ($request->has('new_properties')) {
-            echo $request->category_id . "\n";
             $product->properties()
                 ->saveMany(
                     collect($request->new_properties)
@@ -62,5 +62,55 @@ class ProductController extends Controller
         }
 
         return response()->json($product->with(['images', 'properties'])->get()->last());
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->title = $request->title ?? $product->title;
+        $product->description = $request->description ?? $product->description;
+        $product->quantity = $request->quantity ?? $product->quantity;
+        $product->price = $request->price ?? $product->price;
+        $product->category_id = $request->category_id ?? $product->category_id;
+        $product->save();
+
+        if ($request->has('image_ids')) {
+            $product->images()->sync($request->image_ids);
+        }
+        if ($request->has('new_images')) {
+            $paths = [];
+            collect($request->new_images)->each(function ($image) use (&$paths) {
+                $paths[] = Storage::putFile('images', $image);
+            });
+            if (!empty($paths)) {
+                $product->images()->saveMany(
+                    collect($paths)->map(fn ($path) => new Image(['path' => $path]))
+                );
+            }
+        }
+        if ($request->has('property_ids')) {
+            $product->properties()->sync($request->property_ids);
+        }
+        if ($request->has('new_properties')) {
+            $product->properties()
+                ->saveMany(
+                    collect($request->new_properties)
+                        ->map(fn ($title) => new Property([
+                            'title' => $title,
+                            'category_id' => $request->category_id,
+                        ]))
+                );
+        }
+
+        return response()->json($product->with(['images', 'properties'])->get()->last());
+    }
+
+    /**
+     * operations in respect to relatins:
+     *      - remove from carts 
+     *      - remove related comments 
+     *      - make product_id in order_items null
+     */
+    public function delete(Product $product) {
+
     }
 }
