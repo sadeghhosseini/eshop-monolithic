9686abd02eb650504adbd76f5f40e70753ea7127
Sadegh Hosseini <s.hosseini@develop.local>
2022-01-05 16:21:39 +0330

    adds return types to RelationHelper methods

--- eshop/app/Models/Helpers/RelationshipHelper.php
+++ eshop/app/Models/Helpers/RelationshipHelper.php
@@ -1,25 +1,33 @@
 <?php
+
 namespace App\Models\Helpers;
+
 use Illuminate\Database\Eloquent\Model;
 
-class RelationshipHelper {
-    public static function oneToOneWithFk(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null) {
+class RelationshipHelper
+{
+    public static function oneToOneWithFk(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
         return $model->belongsTo($associatedModelClass, $foreignKey, $primaryKeyOfReferencingTable);
     }
-    
-    public static function oneToOne(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null) {
+
+    public static function oneToOne(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
         return $model->hasOne($associatedModelClass, $foreignKey, $primaryKeyOfReferencingTable);
     }
-    
-    public static function OneToManyWithFk(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null) {
+
+    public static function OneToManyWithFk(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
         return $model->belongsTo($associatedModelClass, $foreignKey, $primaryKeyOfReferencingTable);
     }
-    
-    public static function oneToMany(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null) {
+
+    public static function oneToMany(Model $model, $associatedModelClass, string $foreignKey = null, string $primaryKeyOfReferencingTable = null): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
         return $model->hasMany($associatedModelClass, $foreignKey, $primaryKeyOfReferencingTable);
     }
-    
-    public static function manyToMany(Model $model, $associatedModelClass, string $intermediateTableName = null, string $foreignKeyOfCurrentClassInIntermediateTable = null, string $foreignKeyOfOtherClassInIntermediateTable = null) {
+
+    public static function manyToMany(Model $model, $associatedModelClass, string $intermediateTableName = null, string $foreignKeyOfCurrentClassInIntermediateTable = null, string $foreignKeyOfOtherClassInIntermediateTable = null): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
         return $model->belongsToMany($associatedModelClass, $intermediateTableName, $foreignKeyOfCurrentClassInIntermediateTable, $foreignKeyOfOtherClassInIntermediateTable);
     }
 }
