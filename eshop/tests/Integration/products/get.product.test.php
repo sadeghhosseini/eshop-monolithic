937685c52d7db137fe707515955db757601ecf66
Sadegh Hosseini <s.hosseini@develop.local>
2022-01-13 13:30:46 +0330

    adds integration tests for new routes defined in ProductController

--- eshop/tests/Integration/products/get.product.test.php
+++ eshop/tests/Integration/products/get.product.test.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Models\Category;
+use App\Models\Image;
 use App\Models\Product;
+use App\Models\Property;
+
 use function Pest\Laravel\get;
 use function Tests\helpers\buildUrl;
 use function Tests\helpers\printEndpoint;
@@ -32,4 +35,26 @@ it('returns product with id={id}', function () use ($url) {
 
     expect($response->json())
         ->toEqualCanonicalizing($product->toArray());
+});
+
+
+it ('gets all the properties of certain product', function () use ($url) {
+    $product = Product::factory()->has(Property::factory()->count(5))->create();
+    $response = get("/api/products/$product->id/properties");
+    $response->assertOk();
+    expect($response->json())->toMatchArray($product->properties->toArray());
+});
+
+it ('gets all the images of certain product', function () use ($url) {
+    $product = Product::factory()->has(Image::factory()->count(5))->create();
+    $response = get("/api/products/$product->id/images");
+    $response->assertOk();
+    expect($response->json())->toMatchArray($product->images->toArray());
+});
+
+it ('gets the category of certain product', function () use ($url) {
+    $product = Product::factory()->for(Category::factory())->create();
+    $response = get("/api/products/$product->id/category");
+    $response->assertOk();
+    expect($response->json())->toMatchArray($product->category->toArray());
 });
