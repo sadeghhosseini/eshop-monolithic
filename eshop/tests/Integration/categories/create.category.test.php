f3210d71c387af7cc7af06250110e2551cf07489
Sadegh Hosseini <s.hosseini@develop.local>
2022-01-16 14:22:39 +0330

    renames the integration test file for category routes and modifies the tests in that file to work with new authorization and authentication rules

--- eshop/tests/Integration/categories/create.category.test.php
+++ eshop/tests/Integration/categories/create.category.test.php
@@ -2,8 +2,10 @@
 
 use App\Models\Category;
 use function Pest\Laravel\post;
+use function Tests\helpers\preparePermissions;
 use function Tests\helpers\printEndpoint;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Role;
 
 uses(RefreshDatabase::class);
 $url = '/api/categories';
@@ -11,18 +13,24 @@ beforeAll(function() use ($url) {
     printEndpoint('POST', $url);
 });
 
-
+beforeEach(function() {
+    $this->seed(\Database\Seeders\PermissionSeeder::class);
+    $this->app->make(\Spatie\Permission\PermissionRegistrar::class)->registerPermissions();
+});
 
 it('creates a category without parent', function () use ($url) {
+    // Laravel\Sanctum\Sanctum::actingAs(App\Models\User::factory()->create()->assignRole('admin'));
+    Laravel\Sanctum\Sanctum::actingAs(App\Models\User::factory()->create()->givePermissionTo('add-categories'));
     $category = Category::factory()->make();
     $response = post($url, $category->toArray());
     $response->assertOk();
     $expectedItemsAsArray = $category->makeHidden('parent_id')->toArray();
     expect($response->json())
-        ->toMatchArray($expectedItemsAsArray);
+    ->toMatchArray($expectedItemsAsArray);
 });
 
 it('creates a category with parent', function () use ($url) {
+    Laravel\Sanctum\Sanctum::actingAs(App\Models\User::factory()->create()->givePermissionTo('add-categories'));
     $parentCategory = Category::factory()->create();
     $category = Category::factory(['parent_id' => $parentCategory->id])->make();
     $response = post($url, $category->toArray());
@@ -30,10 +38,11 @@ it('creates a category with parent', function () use ($url) {
     $expectedResponseItemAsArray = $response->json();
     $expectedItemsAsArray = $category->toArray();
     expect($expectedResponseItemAsArray)
-        ->toMatchArray($expectedItemsAsArray);
+    ->toMatchArray($expectedItemsAsArray);
 });
 
 it('returns 400 if input data is not valid', function () use ($url) {
+    Laravel\Sanctum\Sanctum::actingAs(App\Models\User::factory()->create()->givePermissionTo('add-categories'));
     $category = Category::factory(['title' => ''])->make();
     $response = post($url, $category->toArray());
     $response->assertStatus(400);
