ed65d1bd644cfee0abdc97791e662cad3f75add0
Sadegh Hosseini <s.hosseini@develop.local>
2022-01-16 14:19:18 +0330

    adds login and registration endpoints along with corresponding form request validations

--- eshop/app/Http/Controllers/AuthController.php
+++ eshop/app/Http/Controllers/AuthController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterationRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterationRequest $request)
+    {
+        $user = new User();
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->assignRole('customer');
+        $user->save();
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'msg' => 'The provided credentials are incorrect.',
+            ]);
+        }
+
+        return response()->json([
+            'pat' => $user->createToken('login')->plainTextToken, //PAT => PersonalAccessToken
+        ]);
+    }
 }
