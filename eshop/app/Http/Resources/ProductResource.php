35ef0c890ecfc856a80234165e75c7e396e913b3
Sadegh Hosseini <s.hosseini@develop.local>
2022-02-07 15:23:24 +0330

    creates ProductResource

---
+++ eshop/app/Http/Resources/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'quantity' => $this->quantity,
+            'price' => $this->price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'images' => ImageResource::collection($this->whenLoaded('images')),
+            'properties' => PropertyResource::collection($this->whenLoaded('properties')),
+        ];
+    }
+}
